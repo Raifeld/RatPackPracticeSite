e4ed5815bb4e15223196837a6473689c74e368ed
Toni <toniowen.blue@gmail.com>
2017-02-16 09:55:25 +0000

    initial commit

---
+++ bandjava.js
@@ -0,0 +1,15 @@
+var myimgobject = document.images [booktheband]
+
+$("p").click(function(){  // When I click on <p> run this code
+
+	
+ });
+
+
+$('button').mouseenter(function() {
+	$(this).removeClass('hidetext').addclass('showtext')
+})
+
+$('p').click(function() {
+    $(this).slideToggle('slow');
+});
